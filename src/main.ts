a28e7f971038159b64089a0a162f7f10d9f89af4
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-16 15:12:17 +0200

    project files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,66 @@
-import './output.css'
 // Supports weights 100-900
 import '@fontsource-variable/overpass';
 
+const mainCard:HTMLElement | null = document.querySelector(".rating-card");
+const thankYouCard:HTMLElement | null = document.querySelector(".thankyou-card")
 const inputValues = Array.from(document.querySelectorAll('.form__input-label')) ;
-
-const getInputFromForm = () => {
-    console.log('Hello, world!')
-    console.log(inputValues)
-}
 const btnSubmit = document.querySelector('.rating-card__form-btn');
-btnSubmit?.addEventListener('click', getInputFromForm );
+const btnReturn = document.querySelector(".thankyou-card__return-btn")
+let selectedRating:string;
+let resultDisplay:HTMLElement | null = document.querySelector('.result__dynamic')
+
+// When a rating is selected.
+
+inputValues.forEach((value) => {
+    value.addEventListener("click", (e) => {
+        if (e.target.id) {
+            const selectedValue:string =  e.target.id
+           selectedRating = selectedValue 
+        }
+    })
+})
+
+// When the button is clicked
+
+btnSubmit?.addEventListener("click", () => {
+
+
+    if (selectedRating) {
+         thankYouCard?.classList.toggle("hidden")
+    mainCard?.classList.toggle("hidden")
+    
+    switch(selectedRating) {
+        case "rating-1": 
+            resultDisplay.innerText = '1';
+        break;
+        case "rating-2": 
+            resultDisplay.innerText = '2';
+        break;
+        case "rating-3": 
+            resultDisplay.innerText = '3';
+        break;
+        case "rating-4": 
+            resultDisplay.innerText = '4';
+        break;
+        case "rating-5": 
+            resultDisplay.innerText = '5';
+        break;
+        default: 
+            console.log("Invalid selection!")
+    }
+    }
+
+    else  {
+        console.log("You have to select a rating!")
+    }
+
+   
+})
+
+btnReturn?.addEventListener("click", resetCard)
+
+function resetCard() {
+    selectedRating = ""
+    thankYouCard?.classList.toggle("hidden")
+    mainCard?.classList.toggle("hidden")
+}
